e4a2343391d5460d5145ae8040ced7c37dbd897b
lephuclam97 <lamboyboy20@gmail.com>
2019-11-19 10:42:08 +0700

    add api eng to vie

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan =  require('morgan');
-const request = require('request')
-
-const PORT = 8000;
+const EngVieApi = require('./EngVieApiControler');
+const FranVieApi = require('./FranVieApiControler');
+const PORT = 3001;
 
 app.get('/',(req,res)=> {
     var welcome = {
@@ -15,18 +15,7 @@ app.get('/',(req,res)=> {
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use('/engvie',EngVieApi);
+app.use('/franvie',FranVieApi);
 
-app.get("/engvies/:eng", (req,res)=>{
-    let word = req.params.eng;
-    request({
-        url: 'http://localhost:3001/engvie/' + word,
-        method: 'GET',
-        dataType: "json",
-        timeout: 10000,
-    },function(err,data,body) {
-        let obj = JSON.parse(body);
-        console.log(obj.vie)
-        res.send(obj.vie)
-    })
-})
 app.listen(PORT,()=> console.log('Listening on PORT '+ PORT));
